08116d58045265360381acbffd2d991b4989b0fd
byt3_m3 <cbaxter@byt3-mac.local>
2017-10-18 00:14:10 +0300

    Modified Err msgs to accecpt arguments and

--- errors/errmsg.py
+++ errors/errmsg.py
@@ -1,8 +1,16 @@
-def ConnectErrorMSG():
-    __connect_err_msg__ = "Error: Be sure to run the connect() method first"
-    print(__connect_err_msg__)
 
 
-def ConfigErrMsg():
-    __config_err_msg__ = "Error: Unable to get latest configuration"
-    print(__config_err_msg__)
+def ConnectErrorMSG(device):
+    __connect_err_msg__ = "Error: unable command to Device {}\n \
+Be sure to run the connect() method on object".format(device.mgmt_ip)
+    return(__connect_err_msg__)
+
+
+def ConfigErrMsg(device):
+    __config_err_msg__ = "Error: Unable to get latest configuration from {}"\
+        .format(device.mgmt_ip)
+    return(__config_err_msg__)
+
+
+def ListEmptyErrMsg(device):
+    __ListEmptyErrMsg__ = "Error: List is empty, run get_all_int() func"
