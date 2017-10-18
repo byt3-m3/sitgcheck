f28bce8328a82b7cc2eec19d4194bd06084942a1
byt3_m3 <cbaxter@byt3-mac.local>
2017-10-18 22:52:14 +0300

    Testing

--- tester.py
+++ tester.py
@@ -1,12 +1,9 @@
 from modules.device import device as router
+import os
 
 
 sw1 = router("192.168.1.2", "cisco", "cisco")
 r1 = router("192.168.1.123", "cisco", "cisco")
 
-sw1.connect()
 r1.connect()
-print sw1.__name__
-print r1.__name__
-# r1.connect()
-# r1.send_command("show ip route")
+print r1.hostname
