034369aff9483bc6a8da9734b30b3626729cd2c5
byt3_m3 <cbaxter@byt3-mac.local>
2017-10-21 19:24:01 +0300

    testing

--- tester.py
+++ tester.py
@@ -2,6 +2,7 @@ from modules.device import device as router
 
 
 sw1 = router("192.168.1.2", "cisco", "cisco")
+sw2 = router("192.168.1.250", "cisco", "cisco")
 r1 = router("192.168.1.123", "cisco", "cisco")
 
 # sw1.connect()
