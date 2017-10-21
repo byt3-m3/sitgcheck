90301798e2a77851f76957e44ff84c52b87a416e
byt3_m3 <cbaxter@byt3-mac.local>
2017-10-21 16:38:11 +0300

    Created a cdp neighbor scanner method

--- modules/device.py
+++ modules/device.py
@@ -107,6 +107,24 @@ class device:
         else:
             print(errmsg.ConnectErrorMSG(self))
 
+    def get_neighbors(self):
+        """ Returns a list of dicts of the switch's neighbors:
+            {hostname, ip, local_port, remote_port} """
+
+        re_text = "-+\r?\nDevice ID: (.+)\\b\r?\n.+\s+\r?\n\s*IP address:\s+(\d+\.\d+\.\d+\.\d+)\s*\r?\n.*\r?\nInterface: (.+),.+Port ID.+: (.+)\\b\r?\n"
+
+        self.send_command("show cdp neighbors detail")
+
+        neighbors = list()
+        for neighbor in re.findall(re_text, self.ssh_out):
+            n_dict = dict()
+
+            n_dict['hostname'], n_dict['ip'], n_dict['local_port'], n_dict['remote_port'] = neighbor
+
+            neighbors.append(n_dict)
+
+        return neighbors
+
     def get_all_interfaces(self):
         if self.status is True:
             self.send_command("show run | inc interface")
