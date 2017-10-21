f6c10c60d231b96508d0ff95f5122f0e113cd1d4
byt3_m3 <cbaxter@byt3-mac.local>
2017-10-21 23:22:04 +0300

    testing

--- tester.py
+++ tester.py
@@ -2,8 +2,9 @@ from modules.device import device as router
 from modules import l2stig as stig
 
 sw1 = router("192.168.1.2", "cisco", "cisco")
-sw2 = router("192.168.1.250", "cisco", "cisco")
+# sw2 = router("192.168.1.250", "cisco", "cisco")
 # r1 = router("192.168.1.123", "cisco", "cisco")
-
-# print(r1.get_neighbors())
+# sw1.get_snmp_users()
+# print(sw1.int_list)
+# print(sw1.get_snmp_users())
 stig.check_net1623(sw1.parsed_config)
