23e9445a424684b6bc8c8811f5ca9c66e241a8c8
byt3_m3 <cbaxter@byt3-mac.local>
2017-10-21 16:39:39 +0300

    Testing

--- tester.py
+++ tester.py
@@ -4,8 +4,5 @@ from modules.device import device as router
 sw1 = router("192.168.1.2", "cisco", "cisco")
 r1 = router("192.168.1.123", "cisco", "cisco")
 
-r1.connect()
-# r1.get_config()
-print(r1.get_config())
-# print r1.hostname
-# print sw1.hostname
+# sw1.connect()
+print(sw1.get_neighbors())
