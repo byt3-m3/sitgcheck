b6cb184bb9f08928335052a2e3207972c65e90b0
byt3-3 <cbaxtertech@gmail.com>
2017-10-23 22:02:40 +0300

    testing

--- tester.py
+++ tester.py
@@ -1,11 +1,16 @@
 from modules.device import device as router
 from modules import l2stig as stig
+import os
+os.system("clear")
 
-sw1 = router("192.168.1.2", "cisco", "cisco")
+sw1 = router("192.168.1.2", "cisco1", "cisco1", enable_pass="cisco")
 # sw2 = router("192.168.1.250", "cisco", "cisco")
-# r1 = router("192.168.1.123", "cisco", "cisco")
-# sw1.get_snmp_users()
-# print(sw1.int_list)
-# print(sw1.get_snmp_users())
-# stig.check_net1623(sw1.parsed_config)
-stig.check_cat2_net1639(sw1.parsed_config, sw1)
+# r1 = router("192.168.1.123", "c0isco", "cisco")
+#
+
+
+# print(sw1.send_command("show ip route"))
+# print(sw1.get_run())
+
+sw1.get_run()
+stig.check_cat1_nac009(sw1.parsed_config, sw1)
