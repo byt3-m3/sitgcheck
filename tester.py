6a632a43b5baeca0d95d0dcf66486cb6a495f223
byt3_m3 <cbaxter@byt3-mac.local>
2017-10-24 03:46:17 +0300

    Added STIG checker loop

--- tester.py
+++ tester.py
@@ -1,7 +1,7 @@
 from modules.device import device as router
 from modules import l2stig as stig
 import os
-os.system("clear")
+# os.system("clear")
 
 sw1 = router("192.168.1.2", "cisco1", "cisco1", enable_pass="cisco")
 # sw2 = router("192.168.1.250", "cisco", "cisco")
@@ -12,5 +12,16 @@ sw1 = router("192.168.1.2", "cisco1", "cisco1", enable_pass="cisco")
 # print(sw1.send_command("show ip route"))
 # print(sw1.get_run())
 
+# Stig Check
 sw1.get_run()
-stig.check_cat1_nac009(sw1.parsed_config, sw1)
+# devices = [sw1]
+#
+# for device in devices:
+#     stig.check_cat1_nac009(device.parsed_config, sw1)
+#     stig.check_cat1_net0230(device.parsed_config, sw1)
+#     stig.check_cat1_net0600(device.parsed_config, sw1)
+#     stig.check_cat1_net1636(device.parsed_config, sw1)
+#     stig.check_cat1_net1665(device.parsed_config, sw1)
+#     stig.check_cat1_net1623(device.parsed_config, sw1)
+#     stig.check_cat1_net0441(device.parsed_config, sw1)
+#     stig.check_cat2_net1639(device.parsed_config, sw1)
