6e7f69b40643762b48300c4d3f3825f65d8c22cc
byt3_m3 <cbaxter@byt3-mac.local>
2017-10-24 04:08:38 +0300

    testing

--- tester.py
+++ tester.py
@@ -7,13 +7,15 @@ sw1 = router("192.168.1.2", "cisco1", "cisco1", enable_pass="cisco")
 # sw2 = router("192.168.1.250", "cisco", "cisco")
 # r1 = router("192.168.1.123", "c0isco", "cisco")
 #
-
+sw1.get_run()
+stig.check_cat1_net1660(sw1.parsed_config, sw1)
 
 # print(sw1.send_command("show ip route"))
+
 # print(sw1.get_run())
 
 # Stig Check
-sw1.get_run()
+# sw1.get_run()
 # devices = [sw1]
 #
 # for device in devices:
@@ -25,3 +27,4 @@ sw1.get_run()
 #     stig.check_cat1_net1623(device.parsed_config, sw1)
 #     stig.check_cat1_net0441(device.parsed_config, sw1)
 #     stig.check_cat2_net1639(device.parsed_config, sw1)
+#     stig.check_cat1_net1660(device.parsed_config, sw1)
