6d44575da572e8bc501de3c75516704d2cab4d15
byt3_m3 <cbaxter@byt3-mac.local>
2017-10-24 16:46:27 +0300

    testing

--- tester.py
+++ tester.py
@@ -1,21 +1,19 @@
-from modules.device import device as router
+from modules.device import device
 from modules import l2stig as stig
-import os
-# os.system("clear")
 
-sw1 = router("192.168.1.2", "cisco1", "cisco1", enable_pass="cisco")
+
+sw1 = device("192.168.1.2", "cisco1", "cisco1", enable_pass="cisco")
+sw2 = device("192.168.1.2", "cisco", "cisco", enable_pass="cisco")
 # sw2 = router("192.168.1.250", "cisco", "cisco")
 # r1 = router("192.168.1.123", "c0isco", "cisco")
 #
-sw1.get_run()
-stig.check_cat1_net1660(sw1.parsed_config, sw1)
+
 
 # print(sw1.send_command("show ip route"))
+# sw1.get_neighbors()
+print(sw2.enable_pass)
 
-# print(sw1.get_run())
-
-# Stig Check
-# sw1.get_run()
+print(sw1.send_command("show ip int br"))
 # devices = [sw1]
 #
 # for device in devices:
